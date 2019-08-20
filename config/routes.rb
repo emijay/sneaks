a03fd1f7b80524fc8820dde98f0dd7fd21da6ba4
kinskin <md.asshikin@gmail.com>
2019-08-20 11:00:56 +0800

    modified the user table and currently, in the shoe controller, we are able to see the name of the owner of the collection. added the resources :owners

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "shoes#index"
   resources :brands
   resources :sizes
   resources :shoes
+  resources :owners
 end
