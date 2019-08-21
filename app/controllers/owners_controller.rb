2431485b58a1b429b24e4626fa009b48fd310c90
kinskin <md.asshikin@gmail.com>
2019-08-21 13:00:44 +0800

    completed the filter by brands

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,25 +6,45 @@ class OwnersController < ApplicationController
 
     def show
       @id = params[:id].to_s
-
       @brands = Brand.all
-
       @owner = User.where(id: params[:id])
+      @filters = request.query_parameters[:filter]
+      # p !@filters
+      if !@filters
+          p 'going to the no filters'
+          if request.query_parameters[:sort] == "r_date"
+            @shoes = Shoe.where(user_id: params[:id]).order(created_at: :desc)
 
-      if request.query_parameters[:sort] == "r_date"
-        @shoes = Shoe.where(user_id: params[:id]).order(created_at: :desc)
+          elsif request.query_parameters[:sort] == "o_date"
+            @shoes = Shoe.where(user_id: params[:id]).order(created_at: :asc)
 
-      elsif request.query_parameters[:sort] == "o_date"
-        @shoes = Shoe.where(user_id: params[:id]).order(created_at: :asc)
+          elsif request.query_parameters[:sort] == "h_price"
+            @shoes = Shoe.where(user_id: params[:id]).order('cost_price::integer DESC')
 
-      elsif request.query_parameters[:sort] == "h_price"
-        @shoes = Shoe.where(user_id: params[:id]).order('cost_price::integer DESC')
+          elsif request.query_parameters[:sort] == "l_price"
+            @shoes = Shoe.where(user_id: params[:id]).order('cost_price::integer ASC')
 
-      elsif request.query_parameters[:sort] == "l_price"
-        @shoes = Shoe.where(user_id: params[:id]).order('cost_price::integer ASC')
-
+          else
+            @shoes = Shoe.where(user_id: params[:id])
+          end
       else
-        @shoes = Shoe.where(user_id: params[:id])
+          p 'going into the filters'
+          p @filters
+          if request.query_parameters[:sort] == "r_date"
+            @shoes = Shoe.where(user_id: params[:id]).order(created_at: :desc)
+
+          elsif request.query_parameters[:sort] == "o_date"
+            @shoes = Shoe.where(user_id: params[:id]).order(created_at: :asc)
+
+          elsif request.query_parameters[:sort] == "h_price"
+            @shoes = Shoe.where(user_id: params[:id]).order('cost_price::integer DESC')
+
+          elsif request.query_parameters[:sort] == "l_price"
+            @shoes = Shoe.where(user_id: params[:id]).order('cost_price::integer ASC')
+
+          else
+            @shoes = Shoe.where(user_id: params[:id], brand_id: @filters)
+          end
       end
     end
 
@@ -68,8 +88,4 @@ class OwnersController < ApplicationController
       def shoe_params
         params.require(:shoes).permit(:brand_id, :size_id, :name, :color, :release_year, :style, :cost_price, :image_url, :description, :user_id)
       end
-
-      # def filter_params
-      #   params.require(:filters).permit(:brand_ids => [])
-      # end
 end
