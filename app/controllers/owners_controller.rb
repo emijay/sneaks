edff56b3b14493ea5e9ca27515ec15bb6b89fd2e
EliseChia <swedesurrender@gmail.com>
2019-08-21 09:29:02 +0800

    owner show page with owner name and sort function

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,8 +6,7 @@ class OwnersController < ApplicationController
 
     def show
       @id = params[:id].to_s
-      @owner = User.where(user_id: params[:id])
-      # elise added @owner - to show the name on the page
+      @owner = User.where(id: params[:id])
 
       if request.query_parameters[:sort] == "r_date"
         @shoes = Shoe.where(user_id: params[:id]).order(created_at: :desc)
