ed9131df3d5b8a379f0866b8594b69c43e81a568
EliseChia <swedesurrender@gmail.com>
2019-08-23 00:58:16 +0800

    committing before trying experimental card flips

---
+++ app/assets/javascripts/rotate.js
@@ -0,0 +1,9 @@
+    function rotateCard(btn){
+        var $card = $(btn).closest('.card-container');
+        console.log($card);
+        if($card.hasClass('hover')){
+            $card.removeClass('hover');
+        } else {
+            $card.addClass('hover');
+        }
+    }
