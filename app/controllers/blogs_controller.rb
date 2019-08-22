e5c72b969fabcbd96b91fa1c9ffae33da97e228e
kinskin <md.asshikin@gmail.com>
2019-08-22 09:33:30 +0800

    completed the blog controller and a simple view.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,28 +1,40 @@
 class BlogsController < ApplicationController
 
     def index
+      @blogs = Blog.all
     end
 
     def show
+      @blog = Blog.find(params[:id])
     end
 
     def new
     end
 
     def create
+      @blog = Blog.new(blog_params)
+      @blog.save
+
+      redirect_to blogs_path
     end
 
     def edit
+      @blog = Blog.find(params[:id])
     end
 
     def update
+      @blog = Blog.find(params[:id])
+      @blog.update(blog_params)
+
+      redirect_to blogs_path
     end
 
     def destroy
+      @blog = Blog.find(params[:id])
+      @blog.destroy
+
+      redirect_to blogs_path
     end
-
-
-
 
     private
       def blog_params
