c6ff56b6f5a7897320dd49eaabffed3f4e69c8d8
emijay <hilmi.johari@yahoo.com.sg>
2019-08-24 20:24:27 +0800

    testing out js

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -12,30 +12,32 @@ class ShoesController < ApplicationController
     def show
       @shoe = Shoe.find(params[:id])
       @images = [@shoe.first_image_url, @shoe.second_image_url, @shoe.third_image_url, @shoe.fourth_image_url]
-      require 'net/http'
-      require 'json'
-      url = "https://api.suplexed.com/v2/pricer_web.php?p_type=snkrs&p_var1=#{@shoe.style}"
-      uri = URI(url)
 
-      response = Net::HTTP.get(uri)
-      response2 = JSON.parse(response)
-      @arrays = response2["sizes"]
-      @rows = @arrays.select { |item| item["size"] == @shoe.size.size }
 
-      if @rows.empty?
-        @stockx = "No Results"
-        @goat = "No Results"
-        @flightclub = "No Results"
+      # require 'net/http'
+      # require 'json'
+      # url = "https://api.suplexed.com/v2/pricer_web.php?p_type=snkrs&p_var1=#{@shoe.style}"
+      # uri = URI(url)
 
-      else
-        @stockx = "$#{@rows[0]["stockx"]}"
-        @goat = "$#{@rows[0]["goat"]}"
-        @flightclub = "$#{@rows[0]["flightclub"]}"
+      # response = Net::HTTP.get(uri)
+      # response2 = JSON.parse(response)
+      # @arrays = response2["sizes"]
+      # @rows = @arrays.select { |item| item["size"] == @shoe.size.size }
 
-        puts "StockX Price: #{@stockx}"
-        puts "GOAT Price: #{@goat}"
-        puts "FlightClub Price: #{@flightclub}"
-        end
+      # if @rows.empty?
+      #   @stockx = "No Results"
+      #   @goat = "No Results"
+      #   @flightclub = "No Results"
+
+      # else
+      #   @stockx = "$#{@rows[0]["stockx"]}"
+      #   @goat = "$#{@rows[0]["goat"]}"
+      #   @flightclub = "$#{@rows[0]["flightclub"]}"
+
+      #   puts "StockX Price: #{@stockx}"
+      #   puts "GOAT Price: #{@goat}"
+      #   puts "FlightClub Price: #{@flightclub}"
+      # end
     end
 
     def new
